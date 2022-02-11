20346345d3b0cd43f378397ba85c9d8fab964610
judyatohio <js467820@ohio.edu>
2022-02-11 16:13:01 -0500

    worked on Song.cc

--- projects/project-2/Song.cc
+++ projects/project-2/Song.cc
@@ -1,6 +1,7 @@
 #include "Song.h"
 #include "date.h"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -38,9 +39,30 @@ bool Song::operator != (const Song& other)const{
 }
 
 void Song::input(std::istream& ins){
-
+    if (&ins == &cin) {
+        cout << "Input name: ";
+        ins >> name;
+        cout << "Input artist: ";
+        ins >> artist;
+        cout << "Input release date: ";
+        ins >> release;
+    } else {
+        ins >> name;
+        ins >> artist;
+        ins >> release;
+    }
 }
 
 void Song::output(std::ostream& outs)const{
 
+}
+
+istream& operator >> (std::istream& ins, Song& s){
+    s.input(ins);
+    return ins;
+}
+
+ostream& operator << (std::ostream& outs, const Song& s){
+    s.output(outs);
+    return outs;
 }
